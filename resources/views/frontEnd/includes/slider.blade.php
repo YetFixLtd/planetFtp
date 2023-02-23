c1e4969c9619c246bf2a685621a36ed35932c762
Razib Chakraborti <razibc93465@gmail.com>
2023-02-23 13:58:59 +0600

    slider ok

--- resources/views/frontEnd/includes/slider.blade.php
+++ resources/views/frontEnd/includes/slider.blade.php
@@ -1,259 +1,27 @@
 <div class="slider multipost">
     <div class="owl-carousel owl-theme" id="post-slider-multipost">
-        <a onclick="UpdateHits('23593','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/mathil.jpg');"></div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>Mathil</span>
+
+        @php
+            $sliders = DB::table('sliders')
+                ->select('sliders.*')
+                ->get();
+        @endphp
+
+        @forelse ($sliders as $slider)
+            <a href="{{ $slider->productUrl }}">
+                <div class="item">
+                    <div class="image">
+                        <div class="img" style="background-image:url({{ asset($slider->productFile) }});"></div>
+                    </div>
+                    <div class="content">
+                        <div class="title">
+                            <span>{{ $slider->title ?? '' }}</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23610','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/99_songs.jpg');"></div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>99 Songs</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23622','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img"
-                        style="background-image: url('files/the_falcon_and_the_winter_soldier_season_01__2021___hindi___english__episode___05.jpg');">
-                    </div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>The Falcon and the Winter Soldier Season 01 (2021) (Hindi + English) Episode -
-                            05</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23260','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/the_courier__2020_.jpg');">
-                    </div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>The Courier (2020)</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23601','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/kho_kho.jpg');"></div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>Kho Kho</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23602','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/mortal_kombat__2021_.jpg');">
-                    </div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>Mortal Kombat (2021)</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23618','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img"
-                        style="background-image: url('files/bisaat_khel_shatranj_ka_season_01__2021__completed.jpg');">
-                    </div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>Bisaat Khel Shatranj Ka Season 01 (2021) Completed</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23592','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/be_good_or_be_gone.jpg');">
-                    </div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>Be Good or Be Gone</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23527','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/roohi__4k_.jpg');"></div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>Roohi (4K)</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23056','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/roohi.jpg');"></div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>Roohi</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23571','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/well_done_baby.jpg');"></div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>Well Done Baby</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23619','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/nizhal.jpg');"></div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>Nizhal</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23520','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/vakeel_saab.jpg');"></div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>Vakeel Saab</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23521','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/karnan.jpg');"></div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>Karnan</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23523','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img"
-                        style="background-image: url('files/11th_hour_season_01__2021__completed.jpg');">
-                    </div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>11th Hour Season 01 (2021) Completed</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23535','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img"
-                        style="background-image: url('files/the_falcon_and_the_winter_soldier_season_01__2021___hindi___english__episode___04.jpg');">
-                    </div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>The Falcon and the Winter Soldier Season 01 (2021) (Hindi + English) Episode -
-                            04</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23548','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/hello_charlie.jpg');"></div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>Hello Charlie</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23522','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/the_big_bull.jpg');"></div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>The Big Bull</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23543','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/krishnankutty_pani_thudangi.jpg');">
-                    </div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>Krishnankutty Pani Thudangi</span>
-                    </div>
-                </div>
-            </div>
-        </a>
-        <a onclick="UpdateHits('23371','visit')" href="player1e89.html?play=23593">
-            <div class="item">
-                <div class="image">
-                    <div class="img" style="background-image: url('files/flight.jpg');"></div>
-                </div>
-                <div class="content">
-                    <div class="title">
-                        <span>Flight</span>
-                    </div>
-                </div>
-            </div>
-        </a>
+            </a>
+        @empty
+        @endforelse
 
     </div>
 </div>
