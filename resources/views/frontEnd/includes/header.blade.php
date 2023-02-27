c44b4a8744ce25acc7cf8afb0f9dbfd0b692e3ca
Razib Chakraborti <razibc93465@gmail.com>
2023-02-27 12:26:26 +0600

    logo margin

--- resources/views/frontEnd/includes/header.blade.php
+++ resources/views/frontEnd/includes/header.blade.php
@@ -1,12 +1,12 @@
 <header class="style1">
     <div class="top-bar hidden-sm hidden-xs">
         <a class="logotype" href="{{ url('/') }}">
-            <div class="logo"><img src="{{ asset('newFrontEnd/logo.png') }}" alt="logotype" /></div>
+            <div class="logo"><img class="mt-8" src="{{ asset('newFrontEnd/logo.png') }}" alt="logotype" /></div>
             <div class="text"><span style="white-space: nowrap;">FTP SERVER</span></div>
         </a>
     </div>
     <a class="logotype mobile-logotype" href="{{ url('/') }}">
-        <div class="logo"><img src="{{ asset('newFrontEnd/logo.png') }}" alt="logotype" /></div>
+        <div class="logo"><img class="mt-8" src="{{ asset('newFrontEnd/logo.png') }}" alt="logotype" /></div>
         <div class="text"><span style="white-space: nowrap;">FTP SERVER</span></div>
     </a>
     <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#navbar"
